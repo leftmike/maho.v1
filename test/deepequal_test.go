dac77e09f36ad95cd27e951998f62511cb943b59
leftmike <mikemon@gmail.com>
2017-10-28 12:25:21 -0700

    add tests for test.DeepEqual

---
+++ test/deepequal_test.go
@@ -0,0 +1,27 @@
+package test_test
+
+import (
+	"testing"
+
+	"maho/sql"
+	"maho/test"
+)
+
+func TestDeepEqual(t *testing.T) {
+	cases := []struct {
+		a, b interface{}
+		ret  bool
+	}{
+		{1, 2, false},
+		{"abc", "abc", true},
+		{[]string{"abc", "def"}, []string{"abc", "def"}, true},
+		{sql.ID("id"), sql.ID("id"), true},
+		{sql.ID("id"), sql.ID("di"), false},
+	}
+
+	for _, c := range cases {
+		if test.DeepEqual(c.a, c.b) != c.ret {
+			t.Errorf("DeepEqual(%v, %v) got %v want %v", c.a, c.b, !c.ret, c.ret)
+		}
+	}
+}
