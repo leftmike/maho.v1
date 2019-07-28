c6d618ced114801a33f6082998d5a50ee61440dd
leftmike <mikemon@gmail.com>
2019-07-27 17:01:45 -0700

    bbolt: encode/decode rows

--- engine/bbolt/encode.go
+++ engine/bbolt/encode.go
@@ -19,6 +19,12 @@ const (
 	float64ZeroKeyTag = 6
 	float64PosKeyTag  = 7
 	stringKeyTag      = 8
+
+	boolValueTag    = 1
+	int64ValueTag   = 2
+	float64ValueTag = 3
+	stringValueTag  = 4
+	// Value tags must be less than 16.
 )
 
 func encodeUInt64(buf []byte, reverse bool, u uint64) []byte {
@@ -208,12 +214,153 @@ func ParseKey(key []byte, colKeys []engine.ColumnKey, dest []sql.Value) bool {
 	return true
 }
 
-func MakeValue(row []sql.Value) []byte {
+func EncodeVarint(buf []byte, n uint64) []byte {
+	// Copied from github.com/golang/protobuf/proto/encode.go
+	for n >= 1<<7 {
+		buf = append(buf, uint8(n&0x7f|0x80))
+		n >>= 7
+	}
+	return append(buf, uint8(n))
+}
 
-	return nil
+func DecodeVarint(buf []byte) ([]byte, uint64, bool) {
+	// Copied from github.com/golang/protobuf/proto/decode.go
+	var idx int
+	var n uint64
+	for shift := uint(0); shift < 64; shift += 7 {
+		if idx >= len(buf) {
+			return nil, 0, false
+		}
+		b := uint64(buf[idx])
+		idx += 1
+		n |= (b & 0x7F) << shift
+		if (b & 0x80) == 0 {
+			return buf[idx:], n, true
+		}
+	}
+
+	// The number is too large to represent in a 64-bit value.
+	return nil, 0, false
+}
+
+func EncodeZigzag64(buf []byte, n int64) []byte {
+	// Copied from github.com/golang/protobuf/proto/encode.go
+	return EncodeVarint(buf, uint64((uint64(n)<<1)^uint64(n>>63)))
+}
+
+func DecodeZigzag64(buf []byte) ([]byte, int64, bool) {
+	// Copied from github.com/golang/protobuf/proto/decode.go
+	buf, n, ok := DecodeVarint(buf)
+	if !ok {
+		return nil, 0, false
+	}
+	return buf, int64((n >> 1) ^ uint64((int64(n&1)<<63)>>63)), true
+}
+
+func encodeColNumValueTag(buf []byte, cdx int, tag byte) []byte {
+	if cdx == 0 {
+		buf = append(buf, tag)
+	} else if cdx < 16 {
+		buf = append(buf, byte(cdx<<4)|tag)
+	} else {
+		buf = append(buf, 0xF0|tag)
+		buf = EncodeVarint(buf, uint64(cdx))
+	}
+	return buf
+}
+
+func MakeValue(row []sql.Value) []byte {
+	var buf []byte
+	for num, val := range row {
+		if val == nil {
+			continue
+		}
+		switch val := val.(type) {
+		case sql.BoolValue:
+			buf = encodeColNumValueTag(buf, num, boolValueTag)
+			if val {
+				buf = append(buf, 1)
+			} else {
+				buf = append(buf, 0)
+			}
+		case sql.StringValue:
+			b := []byte(val)
+			buf = encodeColNumValueTag(buf, num, stringValueTag)
+			buf = EncodeVarint(buf, uint64(len(b)))
+			buf = append(buf, b...)
+		case sql.Float64Value:
+			buf = encodeColNumValueTag(buf, num, float64ValueTag)
+			buf = encodeUInt64(buf, false, math.Float64bits(float64(val)))
+		case sql.Int64Value:
+			buf = encodeColNumValueTag(buf, num, int64ValueTag)
+			buf = EncodeZigzag64(buf, int64(val))
+		default:
+			panic(fmt.Sprintf("unexpected type for sql.Value: %T: %v", val, val))
+		}
+	}
+	return buf
 }
 
 func ParseValue(buf []byte, dest []sql.Value) bool {
+	var ok bool
+	var u uint64
 
-	return false
+	for len(buf) > 0 {
+		tag := buf[0] & 0x0F
+		num := int(buf[0] >> 4)
+		buf = buf[1:]
+		if num == 16 {
+			buf, u, ok = DecodeVarint(buf)
+			if !ok {
+				return false
+			}
+			num = int(u)
+		}
+
+		var val sql.Value
+		switch tag {
+		case boolValueTag:
+			if len(buf) < 1 {
+				return false
+			}
+			if buf[0] == 0 {
+				val = sql.BoolValue(false)
+			} else {
+				val = sql.BoolValue(true)
+			}
+			buf = buf[1:]
+		case stringValueTag:
+			buf, u, ok = DecodeVarint(buf)
+			if !ok {
+				return false
+			}
+			if len(buf) < int(u) {
+				return false
+			}
+			val = sql.StringValue(buf[:u])
+			buf = buf[u:]
+		case float64ValueTag:
+			if len(buf) < 8 {
+				return false
+			}
+			u = binary.BigEndian.Uint64(buf)
+			val = sql.Float64Value(math.Float64frombits(u))
+			buf = buf[8:]
+		case int64ValueTag:
+			var n int64
+			buf, n, ok = DecodeZigzag64(buf)
+			if !ok {
+				return false
+			}
+			val = sql.Int64Value(n)
+		default:
+			return false
+		}
+
+		if num < len(dest) {
+			dest[num] = val
+		}
+	}
+
+	return true
 }
--- engine/bbolt/encode_test.go
+++ engine/bbolt/encode_test.go
@@ -5,6 +5,8 @@ import (
 	"math"
 	"testing"
 
+	"github.com/golang/protobuf/proto"
+	"github.com/leftmike/maho.old/engine/kvrows/encoding"
 	"github.com/leftmike/maho/engine"
 	"github.com/leftmike/maho/engine/bbolt"
 	"github.com/leftmike/maho/sql"
@@ -493,7 +495,7 @@ func testParseKey(t *testing.T, row []sql.Value, colKeys []engine.ColumnKey) {
 	}
 	for _, ck := range colKeys {
 		num := ck.Number()
-		if !testutil.DeepEqual(dest[num], row[num], nil) {
+		if !testutil.DeepEqual(dest[num], row[num]) {
 			t.Errorf("ParseKey: at %d got %v want %v", num, dest[num], row[num])
 		}
 	}
@@ -575,4 +577,154 @@ func TestParseKey(t *testing.T) {
 
 	testParseKeyReverse(t, row, false)
 	testParseKeyReverse(t, row, true)
+}
+
+func TestEncodeVarint(t *testing.T) {
+	numbers := []uint64{
+		0,
+		1,
+		125,
+		126,
+		127,
+		0xFF,
+		0x100,
+		0xFFF,
+		0x1000,
+		0x7F7F,
+		1234567890,
+		math.MaxUint32,
+		math.MaxUint64,
+	}
+
+	for _, n := range numbers {
+		buf := bbolt.EncodeVarint(nil, n)
+		pbuf := proto.EncodeVarint(n)
+		if !testutil.DeepEqual(buf, pbuf) {
+			t.Errorf("EncodeVarint(%d): got %v want %v", n, buf, pbuf)
+		}
+		ret, r, ok := bbolt.DecodeVarint(buf)
+		if !ok {
+			t.Errorf("DecodeVarint(%v) failed", buf)
+		} else if len(ret) != 0 {
+			t.Errorf("DecodeVarint(%v): got %v want []", buf, ret)
+		} else if n != r {
+			t.Errorf("DecodeVarint(%v): got %d want %d", buf, r, n)
+		}
+	}
+}
+
+func TestEncodeZigzag64(t *testing.T) {
+	numbers := []int64{
+		0,
+		1,
+		125,
+		126,
+		127,
+		128,
+		129,
+		0xFF,
+		0x100,
+		0xFFF,
+		0x1000,
+		0x7F7F,
+		1234567890,
+		10000000000,
+		math.MaxInt32,
+		math.MaxInt64,
+		math.MinInt32,
+		math.MinInt64,
+		-987654321,
+		-1000000000,
+		-125,
+		-126,
+		-127,
+		-128,
+		-129,
+		-0xFF,
+	}
+
+	for _, n := range numbers {
+		buf := encoding.EncodeZigzag64(nil, n)
+		enc := proto.NewBuffer(nil)
+		err := enc.EncodeZigzag64(uint64(n))
+		if err != nil {
+			t.Errorf("proto.EncodeZigzag64(%d) failed with %s", n, err)
+		} else {
+			pbuf := enc.Bytes()
+			if !testutil.DeepEqual(buf, pbuf) {
+				t.Errorf("EncodeZigzag64(%d): got %v want %v", n, buf, pbuf)
+			}
+		}
+		ret, r, ok := encoding.DecodeZigzag64(buf)
+		if !ok {
+			t.Errorf("DecodeZigzag64(%v) failed", buf)
+		} else if len(ret) != 0 {
+			t.Errorf("DecodeZigzag64(%v): got %v want []", buf, ret)
+		} else if n != r {
+			t.Errorf("DecodeZigzag64(%v): got %d want %d", buf, r, n)
+		}
+	}
+}
+
+func TestMakeParseValues(t *testing.T) {
+	cases := []struct {
+		row []sql.Value
+		s   string
+	}{
+		{
+			row: []sql.Value{sql.BoolValue(true)},
+			s:   "true",
+		},
+		{
+			row: []sql.Value{sql.Int64Value(345)},
+			s:   "345",
+		},
+		{
+			row: []sql.Value{sql.Float64Value(987.6543)},
+			s:   "987.6543",
+		},
+		{
+			row: []sql.Value{sql.StringValue("abcdefghijklmnopqrstuvwxyz")},
+			s:   "'abcdefghijklmnopqrstuvwxyz'",
+		},
+		{
+			row: []sql.Value{sql.BoolValue(true), sql.Int64Value(345), sql.Float64Value(987.6543),
+				sql.StringValue("abcdefghijklmnopqrstuvwxyz")},
+			s: "true, 345, 987.6543, 'abcdefghijklmnopqrstuvwxyz'",
+		},
+		{
+			row: []sql.Value{sql.BoolValue(true), nil, sql.Int64Value(345)},
+			s:   "true, NULL, 345",
+		},
+		{
+			row: []sql.Value{nil, nil, nil, sql.StringValue("ABCDEFG")},
+			s:   "NULL, NULL, NULL, 'ABCDEFG'",
+		},
+		{
+			row: []sql.Value{sql.Int64Value(1234567890), sql.StringValue(""), sql.BoolValue(true)},
+			s:   "1234567890, '', true",
+		},
+	}
+
+	for _, c := range cases {
+		buf := bbolt.MakeValue(c.row)
+		dest := make([]sql.Value, len(c.row))
+		ok := bbolt.ParseValue(buf, dest)
+		if !ok {
+			t.Errorf("ParseValue(%s) failed", c.s)
+		} else if !testutil.DeepEqual(c.row, dest) {
+			t.Errorf("ParseValue(%s) got %v want %v", c.s, dest, c.row)
+		}
+
+		var s string
+		for num, val := range dest {
+			if num > 0 {
+				s += ", "
+			}
+			s += sql.Format(val)
+		}
+		if s != c.s {
+			t.Errorf("ParseValue: got %s want %s", s, c.s)
+		}
+	}
 }
