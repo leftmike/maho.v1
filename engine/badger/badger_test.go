b027a8f444772d2fb97a68daf8806e3f4d05af19
leftmike <mikemon@gmail.com>
2020-01-19 13:45:42 -0800

    badger: turn on schema test

--- engine/badger/badger_test.go
+++ engine/badger/badger_test.go
@@ -21,11 +21,11 @@ func TestKV(t *testing.T) {
 
 	test.RunDatabaseTest(t, e, true)
 	test.RunTableTest(t, e)
+	test.RunSchemaTest(t, e)
 	test.RunTableLifecycleTest(t, e)
 	test.RunTableRowsTest(t, e)
 	/*
 		XXX
-		test.RunSchemaTest(t, e)
 		test.RunParallelTest(t, e)
 		test.RunStressTest(t, e)
 	*/
